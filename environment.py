a564e943d4b85f4b9356b3e071c4067dbfd3beb7
Erick880709 <erick880709@hotmail.com>
2021-10-30 16:31:12 -0500

    notificaiones sms y email microservicio en python
    notificaiones sms y email microservicio en python

---
+++ environment.py
@@ -0,0 +1,8 @@
+import os
+
+os.environ["SENDGRID_API_KEY"]="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"
+os.environ["EMAIL_FROM"]="luis2.hernandez@ucp.edu.co"
+os.environ["HASH"]="MicroServicios"
+os.environ["TWILIO_ACCOUNT_SID"]="ACf0753b571f8db0c19fa1f77a6f762758"
+os.environ["TWILIO_AUTH_TOKEN"]="94bb925ab9d204a4282edbe022e56d95"
+os.environ["TWILIO_PHONE_NUMBER"]="+12183927126"
